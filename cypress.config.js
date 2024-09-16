bb70c47b2eb068eca004e7d0030d8adf9cb8701a
Eduardo <eduardo.trandafilov@gmail.com>
2024-09-16 14:00:49 -0300

    inclusão config relatorio  cypress cloud e evidencia video

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "hm37dw",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl:'http://lojaebac.ebaconline.art.br/',
+    video:true,
   },
 });
