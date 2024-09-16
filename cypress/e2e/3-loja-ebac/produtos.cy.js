00fd0f95164ffde3f75811febb5d6df085ecd115
Eduardo <eduardo.trandafilov@gmail.com>
2024-09-16 08:11:39 -0300

    inclusao page objects e massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,47 @@
 /// <reference types="cypress"/>
-
+import produtosPage from "../../support/page-objects/produtos.page";
 describe('Funcionalidade:Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
-    });
+        produtosPage.visitarUrl()
+    })
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            .contains('Aero Daily Fitness Tee')
-            .click()
-
-            cy.get('#tab-title-description > a').should('contain','Descrição')
+        produtosPage.buscarProdutoLista('Atlas Fitness Tank')
+        cy.get('#tab-title-description > a').should('contain','Descrição')
     });
-});
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Atlas Fitness Tank'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain',produto)
+    });
+
+    it('Deve visitar página do produto', () => {
+        produtosPage.visitarProduto('Atlas Fitness Tank')
+        cy.get('.product_title').should('contain','Atlas Fitness Tank')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Atlas Fitness Tank')
+        produtosPage.addProdutoCarrinho('XS','Blue',qtd)
+        
+        cy.get('.woocommerce-message').should('contain',qtd +' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+        
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados=>{
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[1].tamanho,
+                dados[1].cor,
+                dados[1].quantidade)
+            
+            cy.get('.woocommerce-message').should('contain',dados[1].nomeProduto)
+        })
+        
+    });
+})  
+
